2698b0043d6581c7ea1e78b8484bf7d6f13a7a05
yukondude <yukondude@57d292b1-523c-0410-a134-ddcefad2f437>
2008-02-10 00:03:06 +0000

    Added count-web-visits and truncate-domain scripts.

--- bin/conf.bash
+++ bin/conf.bash
@@ -3,8 +3,9 @@
 # Configuration variables.
 #
 # This file is a part of Scripnix <http://code.google.com/p/scripnix/>.
-# Written in 2007 by Dave Rogers <thedude-strudel-yukondude-fullstop-com>.
+# Written in 2008 by Dave Rogers <thedude-strudel-yukondude-fullstop-com>.
 # This script is released into the Public Domain.
 
 # Do not edit these. Override values as needed in user-conf.bash.
+APACHE_LOG='/var/log/apache2/access.log'
 SVN_REPO_DIR='/var/lib/svn'
