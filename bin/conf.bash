a5b008dd2476b6a62df1fe10f43a58de9b2cfae6
yukondude <yukondude@57d292b1-523c-0410-a134-ddcefad2f437>
2008-02-17 18:48:28 +0000

    Added run-once-if and restart-ntp scripts. Handy for computers that aren't on 24x7.

--- bin/conf.bash
+++ bin/conf.bash
@@ -7,11 +7,17 @@
 # Written in 2008 by Dave Rogers <yukondude-strudel-gmail-fullstop-com>.
 # This script is released into the Public Domain.
 
+# Temporary directory (to which pretty much every user has write permissions).
+TMP_DIR='/tmp'
+
 # Apache HTTPD log file.
 APACHE_LOG='/var/log/apache2/access.log'
 
-# Subversion respository parent directory.
-SVN_REPO_DIR='/var/lib/svn'
-
 # Exclusions from find searches.
 FIND_PATH_EXCLUDE='( -path /dev -or -path /proc -or -path /sys ) -prune'
+
+# Default NTP server for ntpdate command.
+NTPDATE_SERVER='time.nrc.ca'
+
+# Subversion respository parent directory.
+SVN_REPO_DIR='/var/lib/svn'
