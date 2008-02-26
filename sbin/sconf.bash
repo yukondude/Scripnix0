5370750707d5c447adaaf955ec3b55f02fc72a1a
yukondude <yukondude@57d292b1-523c-0410-a134-ddcefad2f437>
2008-02-26 01:06:48 +0000

    Added directory/file exclusions to archive script. Moved default archive path directory to /usr/local/sbin. Hid default archive exclusion file.

--- sbin/sconf.bash
+++ sbin/sconf.bash
@@ -8,7 +8,8 @@
 
 # Do not edit these. Override values as needed in suser-conf.bash.
 ARCHIVE_DIR='/var/archive'
-ARCHIVE_PATH_DIR='/etc/archive'
+ARCHIVE_EXCLUSIONS='/usr/local/sbin/archive-exclusions'
+ARCHIVE_PATH_DIR='/usr/local/sbin/archive-paths'
 
 MYSQL_DB_DIR='/var/lib/mysql'
 MYSQL_DUMP_FILE='mysql-dump.tar'
