028ec2efa24941a28887a86fb5ac2a93083f555f
yukondude <yukondude@57d292b1-523c-0410-a134-ddcefad2f437>
2010-08-27 23:27:55 +0000

    Whoops. Turns out you need at least something in FIND_SVN_PATH_EXCLUDE.

--- bin/conf.bash
+++ bin/conf.bash
@@ -18,7 +18,7 @@ APACHE_LOG='/var/log/apache2/access.log'
 
 # Exclusions from find searches.
 FIND_PATH_EXCLUDE='( -path /dev -or -path /proc -or -path /sys ) -prune'
-FIND_SVN_PATH_EXCLUDE=''
+FIND_SVN_PATH_EXCLUDE='-path /dev -prune'
 
 # Default NTP server for ntpdate command.
 NTPDATE_SERVER='time.nrc.ca'
