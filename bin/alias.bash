15edf1603be12c78cbf5c48319f67c80c0b75651
yukondude@gmail.com <yukondude@gmail.com@57d292b1-523c-0410-a134-ddcefad2f437>
2011-05-16 22:43:11 +0000

    Handle both GNU and BSD-style switches.

--- bin/alias.bash
+++ bin/alias.bash
@@ -6,33 +6,46 @@
 # Written in 2008 by Dave Rogers <yukondude-strudel-gmail-fullstop-com>.
 # This script is released into the Public Domain.
 
-alias cp='cp --interactive'
-alias mv='mv --interactive'
-alias rm='rm --interactive'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
 
-alias l='ls --color=tty'
-alias ls='ls --color=tty'
-alias ll='ls -l --color=tty --time-style=long-iso'
-alias la='ls -l --almost-all --color=tty --time-style=long-iso'
-alias lh='ls -l --human-readable --color=tty --time-style=long-iso'
-alias lm='ls -l --block-size=1024K --color=tty --time-style=long-iso'
-alias lt='ls -lt --color=tty --time-style=long-iso'
-alias lrt='ls -lrt --color=tty --time-style=long-iso'
+if [[ "${OS}" == 'darwin' ]]; then
+	alias l='ls -G'
+	alias ls='ls -G'
+	alias ll='ls -Gl'
+	alias la='ls -AGl'
+	alias lh='ls -Ghl'
+	alias lt='ls -Glt'
+	alias lrt='ls -Glrt'
+	
+	alias nst='netstat -an -f inet'
+else
+	alias l='ls --color=tty'
+	alias ls='ls --color=tty'
+	alias ll='ls -l --color=tty --time-style=long-iso'
+	alias la='ls -l --almost-all --color=tty --time-style=long-iso'
+	alias lh='ls -l --human-readable --color=tty --time-style=long-iso'
+	alias lm='ls -l --block-size=1024K --color=tty --time-style=long-iso'
+	alias lt='ls -lt --color=tty --time-style=long-iso'
+	alias lrt='ls -lrt --color=tty --time-style=long-iso'
 
-alias ltt='last -a | tac | tail -n20'
+	alias ltt='last -a | tac | tail -n20'
+	alias nst='netstat --all --tcp --udp --numeric'
+
+	alias ift='sudo iftop -nNPB'
+	alias ipt='sudo iptables -nvL'
+	alias rkup='sudo rkhunter --propupd'
+fi
+
 alias pe='ps -eFlT'
 alias px='ps aux'
-alias nst='netstat --all --tcp --udp --numeric'
 
 alias degrep='egrep --invert-match'
 alias degrep-rcs="degrep '\/(\.svn|CVS)'"
 alias degrep-rcs0="degrep --null-data '\/(\.svn|CVS)'"
 
 alias set-svn-id="svn propset svn:keywords \"Id\""
-
-alias ift='sudo iftop -nNPB'
-alias ipt='sudo iptables -nvL'
-alias rkup='sudo rkhunter --propupd'
 
 # Necessary to enable aliases in shell scripts.
 shopt -s expand_aliases
