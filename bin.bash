447b47d31674c8e10c148ed7693841347c21e6f4
Dave Rogers <thedude@yukondude.com>
2014-04-11 10:44:53 -0700

    Look up the script path rather than hardcoding it.

--- bin.bash
+++ bin.bash
@@ -7,15 +7,16 @@
 # Written in 2007 by Dave Rogers <yukondude-strudel-gmail-fullstop-com>.
 # This script is released into the Public Domain.
 
-bin_dir='/usr/local/bin'
+scriproot="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 system_conf_dir='/etc/scripnix'
 user_conf_dir="${HOME}/.scripnix"
 conf_file='conf.bash'
 
 # Set configuration variables, overriding as necessary.
-source "${bin_dir}/${conf_file}"
+source "${scriproot}/${conf_file}"
 [[ -r "${system_conf_dir}/${conf_file}" ]] && source "${system_conf_dir}/${conf_file}"
 [[ -r "${user_conf_dir}/${conf_file}" ]] && source "${user_conf_dir}/${conf_file}"
 
-source "${bin_dir}/func.bash"
-source "${bin_dir}/alias.bash"
+source "${scriproot}/func.bash"
+source "${scriproot}/alias.bash"
