184d6062962bfc32d05dae572194d96e3ba6ae50
Dave Rogers <thedude@yukondude.com>
2014-04-14 18:30:45 -0700

    Add path and shell config to vagrant account .bashrc as per normal Scripnix installation.

--- dev/box-provisioner.sh
+++ dev/box-provisioner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Vagrant shell box provisioning script for FeedGuild project.
+# Vagrant shell box provisioning script for Scripnix project.
 
 # Don't re-provision the box.
 if [[ -f /var/vagrant-provisioned ]] ; then
@@ -22,5 +22,12 @@ ln --symbolic ${project} Scripnix
 echo "We're on Yukon Time." | tee -a ${log}
 echo "America/Whitehorse" >/etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata >>${log} 2>&1
+
+echo "Set up Scripnix path and shell configuration." | tee -a ${log}
+cd ${home}
+echo >>.bashrc
+echo "# Enable Scripnix" >>.bashrc
+echo 'PATH="/vagrant:${PATH}"' >>.bashrc
+echo "source /vagrant/rc.bash" >>.bashrc
 
 touch /var/vagrant-provisioned
